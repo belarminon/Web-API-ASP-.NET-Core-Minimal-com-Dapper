9207f8fcbf2e38171ee517f2ea6da2f38720185a
Belarmino Nicolau Monteiro Simão <belarnicolau@hotmail.com>
2022-10-27 20:29:00 -0300

    definindo o mapeamento Get, Post, Put e Delete no method de extensao MapTarefasEndpoints

--- TarefasAPI/Endpoints/TarefasEndpoints.cs
+++ TarefasAPI/Endpoints/TarefasEndpoints.cs
@@ -19,6 +19,41 @@ namespace TarefasAPI.Endpoints
 
                 return Results.Ok(tarefas);
             });
+
+            app.MapGet("/tarefas/{id}", async (GetConnection connectionGetter, int id) =>
+            {
+                using var con = await connectionGetter();
+                //var tarefa = con.Get<Tarefa>(id);
+                //if (tarefa is null) return Results.NotFound();
+                //return Results.Ok(tarefa);
+
+                return con.Get<Tarefa>(id) is Tarefa tarefa ? Results.Ok(tarefa) : Results.NotFound();
+            });
+
+            app.MapPost("/tarefas", async (GetConnection connectGetter, Tarefa tarefa) =>
+            {
+                using var con = await connectGetter();
+                var id = con.Insert(tarefa);
+                return Results.Created($"/tarefas/{id}", tarefa);
+            });
+
+            app.MapPut("/tarefas", async (GetConnection connectionGetter, Tarefa Tarefa) =>
+            {
+                using var con = await connectionGetter();
+                var id = con.Update(Tarefa);
+                return Results.Ok();
+            });
+
+            app.MapDelete("/tarefas/{id}", async (GetConnection connectionGetter, int id) =>
+            {
+                using var con = await connectionGetter();
+                var deleted = con.Get<Tarefa>(id);
+
+                if (deleted is null) return Results.NotFound();
+
+                con.Delete(deleted);
+                return Results.Ok(deleted);
+            });
         }
     }
 }
